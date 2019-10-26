d7daa10b8205f4a5a2536f3d40ba5fe2e7a05f45
roman-jordan <rjordan1352@gmail.com>
2019-10-26 13:26:19 -0700

    Knex is a go

---
+++ knexfile.js
@@ -0,0 +1,51 @@
+// Update with your config settings.
+const ENV = process.env.DB_ENV || "development";
+const migrate = `./data/migrations/'${ENV}`;
+const seed = `./data/seeds/'${ENV}`;
+
+module.exports = {
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: "./data/dev.sqlite3"
+    },
+    migrations: {
+      directory: migrate
+    },
+    seeds: {
+      directory: seed
+    }
+  },
+
+  staging: {
+    client: "pg",
+    connection: process.env.DATABASE_URL_STAGING,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations",
+      directory: migrate
+    },
+    seeds: {
+      directory: seed
+    }
+  },
+
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations",
+      directory: migrate
+    },
+    seeds: {
+      directory: seed
+    }
+  }
+};
