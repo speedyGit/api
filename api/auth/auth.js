c2ef387f8cb52856d37015ac20ba96defcf25a51
roman-jordan <rjordan1352@gmail.com>
2019-10-27 10:40:33 -0700

    Resolved Some More Conse Logs

--- api/auth/auth.js
+++ api/auth/auth.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const dbModel = require("./authModel");
 const bcrypt = require("bcryptjs");
 const jwt = require("./preAuth/jwtAccess");
+const HashFactor = process.env.HASH || 8
 
 //validation
 const validateNewUser = require("./preAuth/validNewUser");
@@ -18,7 +19,7 @@ router.get("/", (req, res) => {
 
 router.post("/register", validateNewUser, (req, res) => {
   const user = req.body;
-  const hash = bcrypt.hashSync(user.password, 14);
+  const hash = bcrypt.hashSync(user.password, HashFactor);
   user.password = hash;
   console.log('usersfsre',user)
   dbModel
@@ -44,11 +45,11 @@ router.post("/login", validateLogin, (req, res) => {
     if(user && bcrypt.compareSync(password,user.password)){
       delete user.password
       payload = {
-        ...newUser,
+        ...user,
         token_type: "Basic ",
-        token: jwt.genToken(newUser)
+        token: jwt.genToken(user)
       }
-      res.status(200).json({message:'Login Success'})
+      res.status(200).json({message:'Login Success',...payload})
     } else {
       res.status(401).json({errors:[{password:'Invalid Username Or Password'}]});
     }
--- index.js
+++ index.js
@@ -1,28 +1,29 @@
 //Server Dependencies
-const express = require('express')
-const helmet = require('helmet')
-require('dotenv').config()
+const express = require("express");
+const helmet = require("helmet");
+require("dotenv").config();
 
 //Set Globals
-const PORT = process.env.PORT || 5000
-const ENV = process.env.DB_ENV || 'development'
-const path = require('path')
-global.dbConfig = path.resolve(__dirname + '/data/dbConfig')
+const PORT = process.env.PORT || 5000;
+const ENV = process.env.DB_ENV || "DEFAULT development";
+const HASH = process.env.HASH || "DEFAULT(8)";
+const path = require("path");
+global.dbConfig = path.resolve(__dirname + "/data/dbConfig");
 
 //Require Routes after Globs Declaration to insure they inherate the Globs
-const primaryRouter = require('./api/server')
+const primaryRouter = require("./api/server");
 
 //Init Server
-const server = express()
-server.use(helmet())
-server.use(express.json())
+const server = express();
+server.use(helmet());
+server.use(express.json());
 
 //Bring In the routes
-server.use('/api',primaryRouter)
-
+server.use("/api", primaryRouter);
 
 //Fire the ol'gal up
-server.listen(PORT,()=>{
-    console.log(`\n** Server Listening on port: ${PORT} **\n** Using: ${ENV} Environment **`)
-})
-
+server.listen(PORT, () => {
+  console.log(`\n** Server Listening on port: ${PORT} **\n`);
+  console.log(`** Using: ${ENV} Environment **\n`);
+  console.log(`** FactoringHash: ${HASH} with  Bcrypt **\n`);
+});
