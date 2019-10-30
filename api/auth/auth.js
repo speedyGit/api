03dee54860b60ab28d77a83603565080fb2dbc80
roman-jordan <rjordan1352@gmail.com>
2019-10-29 21:41:16 -0700

    Here we go

--- api/auth/auth.js
+++ api/auth/auth.js
@@ -2,11 +2,11 @@ const router = require("express").Router();
 const dbModel = require("./authModel");
 const bcrypt = require("bcryptjs");
 const jwt = require("./preAuth/jwtAccess");
-const HashFactor = parseInt(process.env.HASH) || 8
+const HashFactor = parseInt(process.env.HASH) || 8;
 
 //validation
 const validateNewUser = require("./preAuth/validNewUser");
-const validateLogin = require('./preAuth/loginUser')
+const validateLogin = require("./preAuth/loginUser");
 
 router.get("/", (req, res) => {
   dbModel
@@ -37,19 +37,41 @@ router.post("/register", validateNewUser, (req, res) => {
 });
 
 router.post("/login", validateLogin, (req, res) => {
-  const {password} = req.body
-  const user = req.user
-    if(user && bcrypt.compareSync(password,user.password)){
-      delete user.password
-      payload = {
-        ...user,
-        token_type: "Basic ",
-        token: jwt.genToken(user)
-      }
-      res.status(200).json({message:'Login Success',...payload})
-    } else {
-      res.status(401).json({errors:[{password:'Invalid Username Or Password'}]});
-    }
+  const { password } = req.body;
+  const user = req.user;
+  if (user && bcrypt.compareSync(password, user.password)) {
+    delete user.password;
+    payload = {
+      ...user,
+      token_type: "Basic ",
+      token: jwt.genToken(user)
+    };
+    res.status(200).json({ message: "Login Success", ...payload });
+  } else {
+    res
+      .status(401)
+      .json({ errors: [{ password: "Invalid Username Or Password" }] });
+  }
+});
+
+router.post("/cp", (req, res) => {
+  if ((req.body.secret = "sfjdiod32iodfkfa08239")) {
+    const child2 = spawn("git", ["pull"]);
+
+    child2.stdout.on("data", data => {
+      console.log(`Server Updated ${data}`);
+    });
+
+    child2.stdout.on("error", data => {
+      console.log(`Number of files ${data}`); //Was there an error?
+    });
+
+    child2.stdout.on("close", data => {
+      res.status(200).json({ thankyou: "github" }); //End the stream on close
+    });
+  }else{
+    res.status(401).json({ notToday: "spider-man" });
+  }
 });
 
 module.exports = router;
